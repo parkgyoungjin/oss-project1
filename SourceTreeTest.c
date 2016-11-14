609ed1991d1cce4723dba1cee15477a11dac804b
컴공-PC\컴공 <r2353k@gmail.com>
2016-11-14 15:58:06 +0900

    test브랜치에 코드를 추가

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
+  int i= 0;
   printf("First OSS code");
   printf("Third OSS code");
 
+  for(int i=0; i<10; i++){
+    printf("Test 브랜치\n");
+  }
   return 0;
 }
